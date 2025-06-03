e960984324eeabeef7b9886da58c61101e6cb63f
Nadson-Cutrim <nadsoncutrimjunior@aluno.uema.br>
2025-06-02 22:35:03 -0300

    Criando o cronômetro

---
+++ src/components/Cronometro/Relogio/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export function Relogio() {
+    return (
+        <React.Fragment>
+        <span>0</span>
+        <span>0</span>
+        <span>:</span>
+        <span>0</span>
+        <span>0</span>
+        </React.Fragment>
+    );
+}
