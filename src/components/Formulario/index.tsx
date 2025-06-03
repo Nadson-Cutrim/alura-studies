0674dd7daba17deaf92b9fe21537b89544d03582
Nadson-Cutrim <nadsoncutrimjunior@aluno.uema.br>
2025-06-02 21:44:59 -0300

    Utilizando props

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -32,7 +32,9 @@ class Formulario extends React.Component {
             required
           />
         </div>
-        <Botao />
+        <Botao texto="Adicionar">
+          Adicionar
+        </Botao>
       </form>
     )
   }
