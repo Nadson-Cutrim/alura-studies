d2b2ddbb267e5269fb54266449bc3c378727ef29
Nadson-Cutrim <nadsoncutrimjunior@aluno.uema.br>
2025-06-02 22:50:52 -0300

    Aplicando css do relógio

--- src/components/Cronometro/Relogio/index.tsx
+++ src/components/Cronometro/Relogio/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-
-export function Relogio() {
+import style from './Relogio.module.scss';
+export default function Relogio() {
     return (
         <React.Fragment>
-        <span>0</span>
-        <span>0</span>
-        <span>:</span>
-        <span>0</span>
-        <span>0</span>
+        <span className= {style.relogioNumero}>0</span>
+        <span className= {style.relogioNumero}>0</span>
+        <span className= {style.relogioDivisao}>:</span>
+        <span className= {style.relogioNumero}>0</span>
+        <span className= {style.relogioNumero}>0</span>
         </React.Fragment>
     );
 }
