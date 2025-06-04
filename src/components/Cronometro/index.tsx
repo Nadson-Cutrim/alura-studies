9300a77c36c53e38513d66a696747d2e7a8df887
Nadson-Cutrim <nadsoncutrimjunior@aluno.uema.br>
2025-06-04 18:53:49 -0300

    useEffect no cornometro

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Botao from "../Botao";
 import Relogio  from "./Relogio";
 import style from './Cronometro.module.scss';
@@ -12,13 +12,17 @@ export default function Cronometro({selecionado}: Props) {
     
     const [tempo, setTempo] = useState<number>();
     
-    if (selecionado?.tempo) {
-        setTempo(tempoParaSegundos(selecionado.tempo));
+    useEffect(() => {
+       if(selecionado?.tempo) {
+        setTempo(tempoParaSegundos(selecionado.tempo))
     }
+    },[selecionado]);
     return(
         <div className={style.cronometro}>
             <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
             Tempo: {tempo}
+
+            
 
             <div className={style.relogioWrapper}>
                 <Relogio />
